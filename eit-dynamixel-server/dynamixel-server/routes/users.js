4b8a00f65693fd4d74104cea0e16531184db9e56
christjt <christannum@gmail.com>
2015-02-13 19:05:51 +0100

    Add http to socket communication

--- eit-dynamixel-server/dynamixel-server/routes/users.js
+++ eit-dynamixel-server/dynamixel-server/routes/users.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var app = require("./../app");
+
 
 /* GET users listing. */
 router.get('/', function(req, res) {
@@ -8,9 +10,15 @@ router.get('/', function(req, res) {
 });
 
 router.post('/', function(req, res){
-    console.log('Someone requested a post');
-    console.log(req.body.move);
-    res.send(req.body.move);
-
+    var connection = app.get("deviceConnection");
+    if(connection == undefined){
+        res.status(400);
+        res.send("Device is not connected")
+    }
+    else{
+        res.status(202);
+        connection.write("Hello from http\n");
+        res.send("Message sent to device")
+    }
 });
 module.exports = router;
